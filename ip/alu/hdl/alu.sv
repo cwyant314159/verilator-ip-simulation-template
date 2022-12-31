3e679945e8473f5b6d6021d7bc923ab8a9458eb6
Cole Wyant <cwyant314159@gmail.com>
2022-12-30 20:04:08 -0600

    Update ALU to use block architecture

--- ip/alu/hdl/alu.sv
+++ ip/alu/hdl/alu.sv
@@ -2,16 +2,16 @@
 module alu #(
     parameter int WIDTH = 6
 ) (
-    input logic              clk,
-    input logic              rst,
+    input logic             clk,
+    input logic             rst,
 
-    input  logic[2:0]        op_in,
-    input  logic[WIDTH-1:0]  a_in,
-    input  logic[WIDTH-1:0]  b_in,
-    input  logic             in_valid,
+    input  logic[2:0]       op_in,
+    input  logic[WIDTH-1:0] a_in,
+    input  logic[WIDTH-1:0] b_in,
+    input  logic            in_valid,
 
-    output logic [WIDTH-1:0] out,
-    output logic             out_valid
+    output logic[WIDTH-1:0] out,
+    output logic            out_valid
 );
 
 import op_code_pkg::*;
@@ -20,9 +20,14 @@ logic [2:0]       op_in_r;
 logic [WIDTH-1:0] a_in_r;
 logic [WIDTH-1:0] b_in_r;
 logic             in_valid_r;
+logic             inv;
+logic [WIDTH-1:0] and_out;
+logic [WIDTH-1:0] or_out;
+logic [WIDTH-1:0] xor_out;
+logic [WIDTH-1:0] not_a_out;
+logic [WIDTH-1:0] not_b_out;
 logic [WIDTH-1:0] result;
 
-// Register all inputs
 always_ff @ (posedge clk, posedge rst)
 begin
     // register all inputs
@@ -36,16 +41,85 @@ begin
     out_valid <= (rst) ? '0 : in_valid_r;
 end
 
-// Compute the result
-assign result = ('0 == in_valid_r)    ? '0                 :
-                (OP_AND   == op_in_r) ?   a_in_r & b_in_r  :
-                (OP_NAND  == op_in_r) ? ~(a_in_r & b_in_r) :
-                (OP_OR    == op_in_r) ?   a_in_r | b_in_r  :
-                (OP_NOR   == op_in_r) ? ~(a_in_r | b_in_r) :
-                (OP_XOR   == op_in_r) ?   a_in_r ^ b_in_r  :
-                (OP_XNOR  == op_in_r) ? ~(a_in_r ^ b_in_r) :
-                (OP_NOT_A == op_in_r) ? ~a_in_r            :
-                (OP_NOT_B == op_in_r) ? ~b_in_r            :
-                                        '0                 ;
+// Determine if the invert control bit needs to be set.
+assign inv = (OP_NAND  == op_in_r) ? '1 :
+             (OP_NOR   == op_in_r) ? '1 :
+             (OP_XNOR  == op_in_r) ? '1 :
+                                     '0 ;
+
+// Output the result based on the op code.
+always_comb
+begin
+    if (!in_valid_r)
+    begin
+        result = '0;
+    end
+    else if (OP_AND == op_in_r || OP_NAND == op_in_r)
+    begin
+        result = and_out;
+    end
+    else if (OP_OR == op_in_r || OP_NOR == op_in_r)
+    begin
+        result = or_out;
+    end
+    else if (OP_XOR == op_in_r || OP_XNOR == op_in_r)
+    begin
+        result = xor_out;
+    end
+    else if (OP_NOT_A == op_in_r)
+    begin
+        result = not_a_out;
+    end
+    else if (OP_NOT_B == op_in_r)
+    begin
+        result = not_b_out;
+    end
+    else
+    begin
+        result = '0;
+    end
+end
+
+// Module instances
+and_block #(
+    .WIDTH(WIDTH)
+) and0 (
+    .invert (inv),
+    .a      (a_in_r),
+    .b      (b_in_r),
+    .out    (and_out)
+);
+
+or_block #(
+    .WIDTH(WIDTH)
+) or0 (
+    .invert (inv),
+    .a      (a_in_r),
+    .b      (b_in_r),
+    .out    (or_out)
+);
+
+xor_block #(
+    .WIDTH(WIDTH)
+) xor0 (
+    .invert (inv),
+    .a      (a_in_r),
+    .b      (b_in_r),
+    .out    (xor_out)
+);
+
+not_block #(
+    .WIDTH(WIDTH)
+) notA_0 (
+    .in     (a_in_r),
+    .out    (not_a_out)
+);
+
+not_block #(
+    .WIDTH(WIDTH)
+) notB_0 (
+    .in     (b_in_r),
+    .out    (not_b_out)
+);
 
 endmodule
